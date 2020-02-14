84a28cd54950cd288b03e8ae713e6c2b2a16d45d
Savan Agrawal <30593809+savan07@users.noreply.github.com>
2020-02-14 11:39:26 +0530

    Update recognize_faces_video.py

--- recognize_faces_video.py
+++ recognize_faces_video.py
@@ -93,12 +93,12 @@ while True:
 		y = top - 15 if top - 15 > 15 else top + 15
 		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
 			0.75, (0, 255, 0), 2)
-		if(name=="Intruder"):
-			unknown_cnt+=1
-			if(unknown_cnt>10):
-				print("Intruder Detected")
-				time.sleep(30)
-				unknown_cnt=0
+		#if(name=="Intruder"):
+		#	unknown_cnt+=1
+		#	if(unknown_cnt>10):
+		#		print("Intruder Detected")
+		#		time.sleep(30)
+		#		unknown_cnt=0
 
 	
 
